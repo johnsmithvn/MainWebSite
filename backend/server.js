2b14840f4b0b9c0bc833f3c71f641217a2245e70
johnsmithvn <johnsmith.vn.1101@gmail.com>
2025-06-14 12:46:02 +0700

    feat(movie): allow setting folder thumbnail from player

--- backend/server.js
+++ backend/server.js
@@ -125,6 +125,7 @@ app.use("/api/movie", require("./api/movie/reset-movie-db"));
 app.use("/api/movie", require("./api/movie/video-cache"));
 app.use("/api/movie", require("./api/movie/favorite-movie"));
 app.use("/api/movie", require("./api/movie/extract-movie-thumbnail"));
+app.use("/api/movie", require("./api/movie/set-thumbnail"));
 
 //
 app.use("/api/music", require("./api/music/scan-music"));
