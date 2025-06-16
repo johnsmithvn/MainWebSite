d7a6f03420a9ca4b3056f9f62e84269ef165ad72
johnsmithvn <johnsmith.vn.1101@gmail.com>
2025-06-16 21:56:50 +0700

    fix: allow login route in security middleware

--- backend/middleware/security.js
+++ backend/middleware/security.js
@@ -1,6 +1,10 @@
 const { SECURITY_KEYS, SECURITY_PASSWORD } = require('../utils/config');
 
 module.exports = function (req, res, next) {
+  if (req.path === '/api/login') {
+    return next();
+  }
+
   const key = (req.query.key || req.body?.key || '').toUpperCase();
   if (key && SECURITY_KEYS.includes(key)) {
     const token = req.headers['x-secure-token'];
@@ -8,5 +12,6 @@ module.exports = function (req, res, next) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
   }
+
   next();
 };
