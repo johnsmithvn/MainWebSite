1b5f9791e2a52fb0cc3e0b6f76762594c7fa59d6
Shu <johnsmith.vn.1101@gmail.com>
2025-08-09 10:44:39 +0700

    fix wraper

--- react-app/src/pages/movie/MovieHome.jsx
+++ react-app/src/pages/movie/MovieHome.jsx
@@ -153,7 +153,7 @@ const MovieHome = () => {
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       {/* Random Sections - First */}
       <div className="bg-gray-50 dark:bg-gray-900 py-6">
-        <div className="max-w-7xl mx-auto px-6">
+        <div className="w-full px-6">
           <MovieRandomSection />
         </div>
       </div>
